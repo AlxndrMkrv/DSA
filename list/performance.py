019a5d68aec6b8baaa5920fdc7b8d5002ab8d07e
Alexander <alexander.makarov@live.com>
2023-07-26 17:02:06 +0300

    merging

--- list/performance.py
+++ list/performance.py
@@ -15,59 +15,7 @@ TestObjects = {"list": list,
                "StlList_pb11": list_pb.StlList
 }
 
-<<<<<<< HEAD
-class Performance:
-    def __init__(self, max_value: int = 1000, steps_number: int = 1000):
-        recursive = lambda x: int(x) if x < 1 else int(recursive(x)*0.95)
-        self.x = [max_value := int(max_value * 0.95)
-                  for _ in range(steps_number) if max_value > 1][::-1]
-
-    @staticmethod
-    def __init_list(TList, n: int):
-        return TList([i for i in range(n)])
-
-    def measure_append_time(self, TList, replay: int = 10):
-        return [timeit(f"[l.append(0) for i in range({x})]", number=replay,
-                       globals={'l': TList()})
-                for x in self.x]
-
-    def measure_get_time(self, TList, replay: int = 10):
-        return [timeit(f"l[{int(x/2)}]", number=replay,
-                       globals={'l': self.__init_list(TList, x)})
-                for x in self.x]
-
-    def measure_size_time(self, TList, replay: int = 10):
-        return [timeit("len(l)", number=replay,
-                       globals={'l': self.__init_list(TList, x)})
-                for x in self.x]
-
-    def measure_insert_time(self, TList, replay: int = 10):
-        return [timeit(f"l.insert({int(x/2)}, 0)", number=replay,
-                       globals={'l': self.__init_list(TList, x)})
-                for x in self.x]
-
-    def measure_pop_time(self, TList, replay: int = 10):
-        return [timeit(f"l.pop({int(x/2)}); l.insert({int(x/2)}, 0)", number=replay,
-                       globals={'l': self.__init_list(TList, x)})
-                for x in self.x]
-
-    def measure_init_time(self, TList, replay: int = 10):
-        return [timeit(f"L(range({x}))", number=replay,
-                       globals={'L': TList})
-                for x in self.x]
-
-    def worker(self, kw: dict):
-        #dict.update(kw, {"values": kw["method"](kw["TList"])})
-        print(f"worker({kw})")
-        return kw
-
-    def __add_plot(self, kw: dict):
-        print(f"__add_plot({kw})")
-        #kw["ax"].plot(self.x, kw["values"], label=kw["label"])
-
-=======
 class Performance (Basic):
->>>>>>> d340b2d (Added array implementation and performance. Tests needed. Made some minor updates)
     def plot(self):
         fig, ax = plt.subplots(2, 3, sharex=True)
         for i, j, title, method in [(0, 0, "Append(x)", self.measure_append_time),
