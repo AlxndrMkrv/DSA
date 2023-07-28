35d305615f7c7d7785c9b85db2324731081fea97
Alexander <alexander.makarov@live.com>
2023-07-28 03:23:50 +0300

    minor changes

--- tree/abstract_tree.hxx
+++ tree/abstract_tree.hxx
@@ -1,22 +1,19 @@
 #pragma once
-#include "common.hxx"
+#include "headers/common.hxx"
 #include <functional>
+#include <algorithm>
 #include <cstring>
 
-#include <pybind11/pybind11.h>
-#include <nanobind/nanobind.h>
 
 template<class T>
-struct Node
-{
+struct Node {
     // methods to implement
     virtual static size_t hash(const T & value) = 0;
 
     //
     Node(const T & value, Node * parent, const unsigned int & children = 0) :
         value(value), parent(parent), _children(children),
-        _offspringLimit(children), _offspring(nullptr)
-    {
+        _offspringLimit(children), _offspring(nullptr) {
         // acquire space for given number of children
         if (children) {
             _offspring = new Node<T> * [children];
@@ -24,22 +21,19 @@ struct Node
     }
 
     ~Node() {
+        for (auto i = 0; i < _children; ++i)
+            delete _offspring[i];
         delete[] _offspring;
     }
 
-    Node<T> & operator[] (int idx = 0)
+    Node<T> & operator[] (const T & value)
     {
-        if (idx < 0)
-            idx += _children;
+        auto idx = search_index(value);
+        if (idx >= _children || _offspring[idx]->value != value)
+            throw py::index_error("value not found");
 
-        if (_children == 0 || idx < 0 || idx > _children)
-            throw py::index_error("index is out of range");
-        else
-            return *_offspring[idx];
+        return *_offspring[idx];
     }
-
-    /* QtCreator's competitor can't chew "auto & at" */
-    Node<T> &(at)(int) = operator[];
 
     Node<T> & getChild (const T & value)
     {
@@ -47,35 +41,21 @@ struct Node
 
     }
 
-    void insert (const T & value)
-    {
+    void insert (const T & value) {
         auto idx = search_index(value);
-        if (hash(value) == hash(*_offspring[idx]))
+        if (idx < _offspring && hash(value) == hash(*_offspring[idx]))
             throw py::index_error("value already presented");
 
-
-
-
-    }
-
-    void add(const T & value) {
         allocate_memory_if_necessary();
-        this->at(_children++) = Node<T>(value, this);
+        if (idx < _offspring)
+            push_apart(idx);
+        idx = new Node<T>(value, this);
     }
 
+    void remove (const T & value) {
 
+    }
 
-    // pop() if -_children <= idx < _children, throw exception otherwise
-    /*Node<T> pop(int idx = -1) {
-        if (idx < 0)
-            idx += _children;
-
-        if (! _children || idx < 0 || idx >= _children)
-            throw py::index_error("pop index out of range");
-        else {
-
-        }
-    }*/
 
 protected:
     void allocate_memory_if_necessary()
@@ -91,8 +71,8 @@ protected:
             const size_t oldAllocatedNodesNumber = _offspringLimit;
             _offspringLimit *= 2;
             _offspring = new Node<T> * [_offspringLimit];
-            std::memmove(_offspring, oldAllocation,
-                         oldAllocatedNodesNumber * sizeof(Node<T> *));
+            std::move(oldAllocation, oldAllocation + oldAllocatedNodesNumber,
+                      _offspring);
             delete[] oldMemory;
         }
     }
@@ -100,8 +80,8 @@ protected:
     // copy all elements backward
     void push_apart(const size_t & idx)
     {
-        for (auto i = _children; i > idx; --i)
-            _offspring[i] = _offspring[i-1];
+        std::move_backward(_offspring + idx, _offspring + _children,
+                           _offspring + _children + 1);
         _offspring[idx] = nullptr;
     }
 
@@ -113,9 +93,9 @@ protected:
         const size_t valueHash = hash(value);
         size_t start, end = 0, _children - 1;
 
-        if (valueHash > hash(_offspring[end]))
+        if (valueHash > hash(_offspring[end]->value))
             return _children;
-        else if (valueHash < hash(_offspring[start]))
+        else if (valueHash < hash(_offspring[start]->value))
             return 0;
 
         auto center = [&start, &end]() -> size_t {return (start+end)/2;};
@@ -137,6 +117,7 @@ protected:
 
     T value;
     Node * parent;
+
 private:
     size_t _children;
     /* Storing a children in array will lead to necessity of grandchildren
@@ -148,15 +129,8 @@ private:
 
 
 template<class T>
-class Tree
-{
-public:
-    Tree(const T & rootValue = T()) : _root(Node<T>(rootValue, nullptr) {}
-
-    Tree<T> & operator[] (int idx = 0) {
-        return
-    }
-
-private:
-    Node<T> _root;
+class Tree : public Node<T> {
+    Tree(const T & value, const size_t &children = 0) :
+        Node<T>(value, nullptr, children) {}
 }
+
--- tree/tree.cxx
+++ tree/tree.cxx
@@ -1,24 +1,29 @@
-#include "common.hxx"
-
-template<class T>
-class Tree
-{
-// Tree will work only with relatively simple objects.
-// Placing [1] and copy([1]) into tree will give different nodes with same content
-};
-
+#include "abstract_tree.hxx"
 #include <functional>
 
+struct PyNode : public Node<py::object> {
+    /* This will work only with relatively simple objects.
+     * Placing [1] and copy([1]) into tree will give different nodes
+     * with same content */
+    static inline size_t hash (const py::object & value) {
+        return (size_t)value.ptr();
+    }
+};
+
+struct PyTree : public PyNode, public Tree<py::object> {};
 
 DEFINE_MODULE(MODULE_NAME(tree), m)
 {
-    m.def("hash", [](py::object obj){return std::hash<py::object>(obj.ptr());});
-    /*
-    py::class_<Tree>(m, "Tree")
-        .def(py::init<>())
-        .def(py::init<py::object>(), py::arg("iterable"))
-        .def("__repr__", &StlArray::__repr__)
-        .def("__len__", [](StlArray &self){return self.size();})
-        .def("__getitem__", &StlArray::__getitem__)
-        ;*/
+    m.def("hash", &PyNode::hash, py::arg("object"));
+
+    py::class_<PyNode>(m, "Node")
+        .def("insert", &PyNode::insert)
+        .def("remove", &PyNode::remove)
+        .def("__getitem__", &PyNode::operator [])
+        ;
+
+    py::class_<PyTree>(m, "Tree")
+        .def(py::init<py::object, size_t>(), py::arg("object"),
+             py::arg("children")=0)
+        ;
 }
